a9024ce3070ed9d108fc26f8e420905b8b294aa3
Lukas Siemon <lukas.siemon@blackflux.com>
2023-08-04 12:10:51 -0700

    fix: updated deps

--- gardener.js
+++ gardener.js
@@ -1,10 +1,9 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import gardener from 'js-gardener';
-
-import { fileURLToPath } from 'url';
+/* eslint-disable import/no-extraneous-dependencies */
 import process from 'process';
+import gardener from 'js-gardener';
+import fs from 'smart-fs';
 
-if (process.argv[1] === fileURLToPath(import.meta.url)) {
+if (process.argv[1] === fs.filename(import.meta.url)) {
   gardener({
     skip: []
   }).catch(() => process.exit(1));
